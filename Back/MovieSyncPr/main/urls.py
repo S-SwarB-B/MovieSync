a5b530b4cdcb9d597a08aefe7e7616665922030b
EgorSavukov <egor.sav0306@mail.ru>
2025-05-27 21:42:08 +0300

    Инициализвация сайта в Django, первые правки (0.1.10.2)

--- Back/MovieSyncPr/main/urls.py
+++ Back/MovieSyncPr/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.main_screen_no_auth, name='main_screen_no_auth'),
+    path('AllFilmsNoAuth', views.all_films_no_auth, name='all_films_no_auth'),
 ]
--- Back/MovieSyncPr/main/views.py
+++ Back/MovieSyncPr/main/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("<h4>Ура победа тестовый запуск удался.</h4>")
+def main_screen_no_auth(request):
+    return render(request, 'main/MainScreenNoAuth.html')
+
+def all_films_no_auth(request):
+    return render(request, 'main/AllFilmsNoAuth.html')
+
