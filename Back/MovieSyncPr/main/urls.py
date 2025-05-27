b8a77c159581008ad86264e345c9591526118a21
Artem <artemka4837@gmail.com>
2025-05-27 23:56:53 +0300

    Сайт полностью перенесен на Django (0.1.11)

--- Back/MovieSyncPr/main/urls.py
+++ Back/MovieSyncPr/main/urls.py
@@ -3,4 +3,8 @@ from .import views
 urlpatterns = [
     path('', views.main_screen_no_auth, name='main_screen_no_auth'),
     path('AllFilmsNoAuth', views.all_films_no_auth, name='all_films_no_auth'),
+    path('MainScreen', views.main_screen, name='main_screen'),
+    path('AllFilms', views.all_films, name='all_films'),
+    path('Favorite', views.favorite, name='favorite'),
+    path('Profile', views.profile, name='profile'),
 ]
--- Back/MovieSyncPr/main/views.py
+++ Back/MovieSyncPr/main/views.py
@@ -7,3 +7,15 @@ def main_screen_no_auth(request):
 def all_films_no_auth(request):
     return render(request, 'main/AllFilmsNoAuth.html')
 
+def main_screen(request):
+    return render(request, 'main/MainScreen.html')
+
+def all_films(request):
+    return render(request, 'main/AllFilms.html')
+
+def favorite(request):
+    return render(request, 'main/Favorite.html')
+
+def profile(request):
+    return render(request, 'main/Profile.html')
+
