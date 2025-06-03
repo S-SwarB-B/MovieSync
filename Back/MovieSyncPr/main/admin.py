aba55d6d41bfd29fcecee45078cc3811cbc90838
Artem <artemka4837@gmail.com>
2025-06-03 22:13:56 +0300

    Добавлен вывод жанров на страницу фильма. Немного изменена структура страницы фильмов и карточек. Исправлены небольшие недочеты (0.8.1)

--- Back/MovieSyncPr/main/admin.py
+++ Back/MovieSyncPr/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Films
+from .models import Films, Genre
 
 admin.site.register(Films)
+admin.site.register(Genre)
--- Back/MovieSyncPr/main/models.py
+++ Back/MovieSyncPr/main/models.py
@@ -1,14 +1,35 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 # Create your models here.
 from django.db import models
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
 class Films(models.Model):
     title = models.CharField('Название фильма',max_length=150)
     description = models.CharField('Описание фильма', max_length=350)
     picture = models.ImageField(upload_to='')
-    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    genres = models.ManyToManyField(Genre, related_name='films', blank=True)
+    rating = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0.0),
+            MaxValueValidator(10.0)
+        ]
+    )
     video_url = models.CharField('Ссылка на видео или файл', max_length=255, blank=True)
 
     def __str__(self):
@@ -17,4 +38,9 @@ class Films(models.Model):
     class Meta:
         verbose_name = 'Фильм'
         verbose_name_plural = 'Фильмы'
+
+
+
+
+
 
